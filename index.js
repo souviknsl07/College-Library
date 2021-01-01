4c88674a1819ba3649100c702a671bea126e75d7
Souvik Nath <souviknsl07@gmail.com>
2021-01-01 18:36:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+function Book(name, author, type) {
+  this.name = name;
+  this.author = author;
+  this.type = type;
+}
+
+function Display() {}
+
+//add methods to display prototype
+Display.prototype.add = function (book) {
+  let tableBody = document.getElementById("tableBody");
+  let uiString = `
+        <tr>
+            <td>${book.name}</td>
+            <td>${book.author}</td>
+            <td>${book.type}</td>
+        </tr>
+    `;
+  tableBody.innerHTML += uiString;
+};
+
+//Implement the clear function
+Display.prototype.clear = function () {
+  let libraryForm = document.getElementById("libraryForm");
+  libraryForm.reset();
+};
+
+//Implement the validate function
+Display.prototype.validate = function (book) {
+  if (book.name.length < 2 || book.author.length < 2) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+Display.prototype.show = function (type, displayMessage) {
+  let message = document.getElementById("message");
+  message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                            <strong>Message </strong> ${displayMessage}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>`;
+  setTimeout(() => {
+    message.innerHTML = "";
+  }, 2000);
+};
+
+//add submit event listener to libraryForm
+let libraryForm = document.getElementById("libraryForm");
+libraryForm.addEventListener("submit", libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+  let name = document.getElementById("bookName").value;
+  let author = document.getElementById("author").value;
+  let fiction = document.getElementById("fiction");
+  let programming = document.getElementById("programming");
+  let engineering = document.getElementById("engineering");
+  let type;
+
+  if (fiction.checked) {
+    type = fiction.value;
+  } else if (programming.checked) {
+    type = programming.value;
+  } else if (engineering.checked) {
+    type = engineering.value;
+  }
+
+  let book = new Book(name, author, type);
+  console.log(book);
+
+  let display = new Display();
+  if (display.validate(book)) {
+    display.add(book);
+    display.clear();
+    display.show("success", "Your book has been added");
+  } else {
+    display.show("danger", "Sorry book cannot be added");
+  }
+
+  e.preventDefault();
+}
